060eda05eba870422183d7096ec5355d396fa835
bhanupriyagupta <89582793+bhanupriyagupta@users.noreply.github.com>
2023-05-08 11:29:09 +0530

    pushing customer entity

--- personal-banking-services/src/main/java/Entity/Customer.java
+++ personal-banking-services/src/main/java/Entity/Customer.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -13,14 +13,41 @@ import java.util.Date;
 @AllArgsConstructor
 public class Customer {
 
+    @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_sequence"
+    )
+    @Column(name = "customer_id", nullable = false)
     private Integer customerId;
+
+    @Column(name = "customer_name", nullable = false)
     private String customerName;
+
+    @Column(name = "customer_phone", nullable = false)
     private String customerPhone;
+
+    @Column(name = "customer_Email", nullable = false)
     private String customerEmail;
+
+    @Column(name = "date_became_customer", nullable = false)
     private Date dateBecameCustomer;
+
+    @Column(name = "login", nullable = false)
     private String login;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "other_details")
     private String otherDetails;
+
+    @Column(name = "customer_types_code", nullable = false)
     private String customerTypesCode;
 
 }
